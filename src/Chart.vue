cbdb70acacd9d8ff15b56af7c7789f4d36a491cd
emigpa <emigpa@gmail.com>
2018-02-08 14:20:50 -0300

    Deleted unused charts

--- src/Chart.vue
+++ src/Chart.vue
@@ -1,29 +1,18 @@
 <template>
   <v-container fluid>
-    <v-layout row wrap>
-      <v-flex sm8>
-        <v-card height="100%" id="leftcard">
-          <v-card-title class="pb-4" primary-title>
-            <div>
-              <h1 class="headline">{{ autor }}</h1>
-                <h2 class="subheading">Escuela de Humanidades</h2>
-                <h2 class="subheading">Artículos publicados: {{ cantArticulos }}</h2>
-                <!-- <h2 class="subheading">Datos de SIGEVA-UNSAM</h2> -->
-            </div>
-          </v-card-title>
-          <v-card-media class="pa-3" contain>
-            <column-chart :stacked="true" :data="stacked.data" :colors="stacked.colors"></column-chart>
-          </v-card-media>
-        </v-card>
-      </v-flex>
-      <v-flex sm4>
-        <v-card class="pa-3" height="100%">
-          <v-card-media class="pa-3 d-block" contain id="perchart"></v-card-media>
-          <v-divider></v-divider>
-          <v-card-media class="pl-3 pr-3 d-block" id="piechart" contain></v-card-media>
-        </v-card>
-      </v-flex>
-    </v-layout>
+    <v-card height="100%" id="leftcard">
+      <v-card-title class="pb-4" primary-title>
+        <div>
+          <h1 class="headline">{{ autor }}</h1>
+          <h2 class="subheading">Escuela de Humanidades</h2>
+          <h2 class="subheading">Artículos publicados: {{ cantArticulos }}</h2>
+          <!-- <h2 class="subheading">Datos de SIGEVA-UNSAM</h2> -->
+        </div>
+      </v-card-title>
+      <v-card-media class="pa-3" contain>
+        <column-chart :stacked="true" :data="stacked.data" :colors="stacked.colors"></column-chart>
+      </v-card-media>
+    </v-card>
   </v-container>
 </template>
 <script>
@@ -72,62 +61,9 @@ export default {
         ]
       }
     }
-  },
-  mounted () {
-    this.setChart()
-  },
-  methods: {
-    setChart () {
-      // console.log(this.sigevaData)
-      // const lineChart = new Chart({
-      //   parent: '#linechart', // or a DOM element
-      //   data: this.lineData,
-      //   type: 'line', // or 'line', 'scatter', 'pie', 'percentage'
-      //   region_fill: 1,
-      //   // height: 250,
-      //   colors: ['blue'],
-      //   format_tooltip_x: d => (d + '').toUpperCase(),
-      //   format_tooltip_y: d => `${d} Artículos`,
-      //   title: 'Artículos publicados'
-      //
-      // })
-      const perChart = new Chart({
-        parent: '#perchart', // or a DOM element
-        data: this.perData,
-        type: 'percentage', // or 'line', 'scatter', 'pie', 'percentage'
-        // height: document.getElementById('leftcard').offsetHeight / 2 - 200,
-        title: 'Área de conocimiento'
-        // format_tooltip_x: d => (d + '').toUpperCase(),
-        // format_tooltip_y: d => d
-      })
-      const pieChart = new Chart({
-        parent: '#piechart', // or a DOM element
-        data: this.pieData,
-        type: 'percentage', // or 'line', 'scatter', 'pie', 'percentage'
-        height: document.getElementById('leftcard').offsetHeight / 4,
-        // height: 150,
-        title: 'Revistas'
-        // format_tooltip_x: d => (d + '').toUpperCase(),
-        // format_tooltip_y: d => d
-      })
-    }
   }
 }
 
 </script>
 <style>
-/* .text-muted {
-  font-size: 8px;
-} */
-.graph-stats-container {
-  flex-direction: column;
-  justify-content: flex-end;
-}
-.chart-container {
-  display: flex;
-  flex-direction: column;
-}
-/* .frappe-chart {
-  justify-items: center;
-} */
 </style>
