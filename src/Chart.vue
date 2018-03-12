6cec58addd036a3247433898145385324d202ccd
emigpa <emigpa@gmail.com>
2018-03-12 15:46:56 -0300

    Changed scale in chart to discrete.

--- src/Chart.vue
+++ src/Chart.vue
@@ -5,7 +5,7 @@
           <h1 class="subheading">PUBLICACIONES</h1>
       </v-card-title>
           <v-card-media class="pl-5 pr-5 pb-3 pt-3" contain>
-            <column-chart :stacked="true" :data="stacked.data" :colors="stacked.colors" legend="right"></column-chart>
+            <column-chart :library="stacked.options" :stacked="true" :data="stacked.data" :colors="stacked.colors" legend="right"></column-chart>
           </v-card-media>
     </v-card>
   </v-container>
@@ -23,7 +23,15 @@ export default {
           {name: 'Trabajos en eventos', data: this.sigevadata.tepLength},
           {name: 'Otras producciones científicas', data: this.sigevadata.demLength}
         ],
-        colors: ["rgba(37, 47, 48, 0.80)", "rgba(49, 86, 89, 0.80)","rgba(41, 119, 159, 0.80)","rgba(187, 171, 121, 0.80)","rgba(198, 224, 255, 0.80)"]
+        colors: ["rgba(37, 47, 48, 0.80)", "rgba(49, 86, 89, 0.80)","rgba(41, 119, 159, 0.80)","rgba(187, 171, 121, 0.80)","rgba(198, 224, 255, 0.80)"],
+        options: { scales:
+          { yAxes:
+            [{ ticks: {
+              min: 0,
+              stepSize: 1 }
+            }]
+          }
+        }
       }
     }
   }
