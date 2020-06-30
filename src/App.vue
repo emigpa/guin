ecde67d32a1fe5b73d7118dc8c2aeb6ff25ea3b0
emigpa <emigpa@gmail.com>
2020-06-30 11:49:11 -0300

    Removed 2014 added 2020.

--- src/App.vue
+++ src/App.vue
@@ -55,44 +55,44 @@ export default {
           const getLength = (item, anio) =>
             sigevadata[item].filter(x => x.anio === anio).length
           sigevadata.articulosLength = {
-            '2014': getLength('articulos', 2014),
             '2015': getLength('articulos', 2015),
             '2016': getLength('articulos', 2016),
             '2017': getLength('articulos', 2017),
             '2018': getLength('articulos', 2018),
-            '2019': getLength('articulos', 2019)
+            '2019': getLength('articulos', 2019),
+            '2020': getLength('articulos', 2020),
           }
           sigevadata.librosLength = {
-            '2014': getLength('libros', 2014),
             '2015': getLength('libros', 2015),
             '2016': getLength('libros', 2016),
             '2017': getLength('libros', 2017),
             '2018': getLength('libros', 2018),
-            '2019': getLength('libros', 2019)
+            '2019': getLength('libros', 2019),
+            '2020': getLength('libros', 2020)
           }
           sigevadata.pdlLength = {
-            '2014': getLength('pdl', 2014),
             '2015': getLength('pdl', 2015),
             '2016': getLength('pdl', 2016),
             '2017': getLength('pdl', 2017),
             '2018': getLength('pdl', 2018),
-            '2019': getLength('pdl', 2019)
+            '2019': getLength('pdl', 2019),
+            '2020': getLength('pdl', 2020)
           }
           sigevadata.tepLength = {
-            '2014': getLength('tep', 2014),
             '2015': getLength('tep', 2015),
             '2016': getLength('tep', 2016),
             '2017': getLength('tep', 2017),
             '2018': getLength('tep', 2018),
-            '2019': getLength('tep', 2019)
+            '2019': getLength('tep', 2019),
+            '2020': getLength('tep', 2020)
           }
           sigevadata.demLength = {
-            '2014': getLength('dem', 2014),
             '2015': getLength('dem', 2015),
             '2016': getLength('dem', 2016),
             '2017': getLength('dem', 2017),
             '2018': getLength('dem', 2018),
-            '2019': getLength('dem', 2019)
+            '2019': getLength('dem', 2019),
+            '2020': getLength('dem', 2020)
           }
           this.sigevadata = sigevadata
           // console.log(
